7e469e3fcb0cc07c08498946ccb52b237bc598bb
Nikita Fokin <nikita.fokin0123@gmail.com>
2025-02-11 21:57:02 +0500

    add matherial

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
-import "./style.css";
 import * as THREE from "three";
 import { OrbitControls } from "three/examples/jsm/controls/OrbitControls.js";
 import * as dat from "dat.gui";
+import CANNON from "cannon";
 
 /**
  * Debug
@@ -33,10 +33,47 @@ const environmentMapTexture = cubeTextureLoader.load([
 ]);
 
 /**
+ * Physics
+ */
+//WoRld
+const world = new CANNON.World();
+world.gravity.set(0, -9.82, 0);
+
+// Matherial
+const concreteMaterial = new CANNON.Material("concrete");
+const plasticMaterial = new CANNON.Material("plastic");
+
+const concretePlasticContactMaterial = new CANNON.ContactMaterial(
+  concreteMaterial,
+  plasticMaterial,
+  { friction: 0.1, restitution: 0.7 }
+);
+world.addContactMaterial(concretePlasticContactMaterial);
+
+//Sphere
+const sphereShape = new CANNON.Sphere(0.5);
+const sphereBody = new CANNON.Body({
+  mass: 1,
+  position: new CANNON.Vec3(0, 3, 0),
+  shape: sphereShape,
+  material: plasticMaterial,
+});
+world.addBody(sphereBody);
+
+//FLoor
+const floorShape = new CANNON.Plane();
+const floorBody = new CANNON.Body();
+floorBody.material = concreteMaterial;
+floorBody.mass = 0;
+floorBody.addShape(floorShape);
+floorBody.quaternion.setFromAxisAngle(new CANNON.Vec3(-1, 0, 0), Math.PI * 0.5);
+world.addBody(floorBody);
+
+/**
  * Test sphere
  */
 const sphere = new THREE.Mesh(
-  new THREE.SphereBufferGeometry(0.5, 32, 32),
+  new THREE.SphereGeometry(0.5, 32, 32),
   new THREE.MeshStandardMaterial({
     metalness: 0.3,
     roughness: 0.4,
@@ -51,7 +88,7 @@ scene.add(sphere);
  * Floor
  */
 const floor = new THREE.Mesh(
-  new THREE.PlaneBufferGeometry(10, 10),
+  new THREE.PlaneGeometry(10, 10),
   new THREE.MeshStandardMaterial({
     color: "#777777",
     metalness: 0.3,
@@ -134,9 +171,15 @@ renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2));
  * Animate
  */
 const clock = new THREE.Clock();
+let oldElapsedTime = 0;
 
 const tick = () => {
   const elapsedTime = clock.getElapsedTime();
+  const deltaTime = elapsedTime - oldElapsedTime;
+  oldElapsedTime = elapsedTime;
+  //Update physics world
+  world.step(1 / 60, deltaTime, 3);
+  sphere.position.copy(sphereBody.position);
 
   // Update controls
   controls.update();
